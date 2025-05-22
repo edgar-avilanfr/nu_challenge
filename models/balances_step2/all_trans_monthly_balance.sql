644ad7cfe2f85b0d9f79e2ed9d4c0b4c072c2b40
Edgar Avilan <edgaravilanfr@gmail.com>
2025-05-22 17:01:22 +0000

    all combiuned

--- models/balances_step2/all_trans_monthly_balance.sql
+++ models/balances_step2/all_trans_monthly_balance.sql
@@ -2,15 +2,20 @@ with
     nonpix_agg as (select * from {{ ref("nonpix_trans_combined") }}),
     pix_agg as (select * from {{ ref("pix_trans_monthly_agg") }})
 
-select action_year, action_month, year_month, account_id,
-sum(total_ins) as total_ins,
-sum(total_outs) as total_outs,
-sum(monthly_balance) as monthly_balance
-from (
-select *
-from nonpix_agg
-UNION ALL
-select *
-from pix_agg
-)
+select
+    action_year,
+    action_month,
+    year_month,
+    account_id,
+    sum(total_ins) as total_ins,
+    sum(total_outs) as total_outs,
+    sum(monthly_balance) as monthly_balance
+from
+    (
+        select *
+        from nonpix_agg
+        union all
+        select *
+        from pix_agg
+    )
 group by action_year, action_month, year_month, account_id
